09b4ff10b54a4e58d4bfec946d8f821e15e76ec8
AatibAbdullah1 <59493408+AatibAbdullah1@users.noreply.github.com>
2021-01-17 16:00:53 -0800

    Add files via upload

---
+++ Home Page/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Text
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Comic Sans MS", 10F);
+            this.richTextBox1.Location = new System.Drawing.Point(283, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(410, 274);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.UseWaitCursor = true;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(395, 317);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(352, 76);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(128, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 45);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "block";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 22);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(103, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 45);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Add Friend";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 45);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Remove Friend";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 352);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(98, 44);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Unblock";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(10, 411);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(239, 22);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(264, 292);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 45);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Lobbies";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(264, 397);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 51);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Join Lobby";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(264, 339);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 54);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Create Lobby";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(357, 399);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 49);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Exit Lobby";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(713, 34);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 60);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Status";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(184, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Friends";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(16, 109);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(162, 237);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Text
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Text";
+            this.Text = "Text-Box";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Text_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ Home Page/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    //public class FileMissing : Exception
+    //{
+    //    public FileMissing(string message) : base("File is missing Converse.txt")//base(message)
+    //    {
+    //    }
+    //}
+
+    public partial class Text : Form
+    {
+        string namelin = "Jack";
+        int lobby_num = 0;
+        public Text()
+        {
+            InitializeComponent();
+
+            /*
+            int users = 4;
+            int usernum = 0;
+
+
+            string Users = "";
+            for (int a = 0; a < users - 1; a++)
+            {
+                usernum++;
+                string j = usernum.ToString();
+                Users = Users + "user" + j + ",";
+
+            }
+            usernum++;
+            string s = usernum.ToString();
+            Users = Users + "user" + s + "!";
+            lobby_num++;
+            string lobbyid = lobby_num.ToString();
+
+            String Users_lobby = "\nLobby" + lobbyid + ": " + Users + "\n";
+
+
+            File.AppendAllText("Converse.txt", Users_lobby);
+
+            StreamReader file = new StreamReader("Converse.txt");
+
+            string line = file.ReadLine();
+            while (line != null)
+            {
+                if (line != "Lobby" + lobbyid + ": " + Users)
+                {
+                    richTextBox1.AppendText(line + "\n");
+
+                }
+                line = file.ReadLine();
+            }
+            file.Close();
+            */
+
+
+            // string filetext = File.R
+            //richTextBox1.Text = filetext;
+            richTextBox1.SelectionStart = richTextBox1.TextLength;
+            richTextBox1.ScrollToCaret();
+        }
+
+        private void Text_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+            string looker = textBox1.Text;
+            int i;
+            int count = 0;
+            int position = 0;
+            string comm;
+            for (i = 0; i < looker.Length; i++)
+            {
+                comm = looker.Substring(0, i);
+
+                if (comm == "add ")
+                {
+                    break;
+                }
+                if (comm == "del ")
+                {
+
+                    break;
+                }
+                if (comm == "block ")
+                {
+
+                    break;
+                }
+                if (comm == "unblock ")
+                {
+
+                    break;
+                }
+                count++;
+            }
+            string finder;
+            comm = looker.Substring(0, count);
+
+            if (comm == "add ")
+            {
+
+                finder = looker.Substring(count);
+                StreamReader file = new StreamReader("Users.txt");
+                string friendly = file.ReadLine();
+                while (friendly != null)
+                {
+                    if (finder == friendly)
+                    {
+                        File.AppendAllText("MyFriends.txt", finder + "\n");
+                    }
+                    friendly = file.ReadLine();
+                }
+                file.Close();
+                textBox1.Text = "";
+            }
+            else if (comm == "del ")
+            {
+
+                finder = looker.Substring(count);
+                StreamReader file = new StreamReader("MyFriends.txt");
+                string deleter = file.ReadLine();
+                while (deleter != null)
+                {
+                    if (deleter != finder)
+                    {
+                        File.AppendAllText("deletefriend.txt", deleter);
+                    }
+                    deleter = file.ReadLine();
+                }
+                file.Close();
+
+                FileStream fileStream = File.Open("MyFriends.txt", FileMode.Open);
+                fileStream.SetLength(0);
+                fileStream.Close();
+
+                StreamReader newfile = new StreamReader("deletefriend.txt");
+                string restore = newfile.ReadLine();
+                while (restore != null)
+                {
+
+                    File.AppendAllText("MyFriends.txt", restore);
+                    restore = newfile.ReadLine();
+                }
+                newfile.Close();
+                textBox1.Text = "";
+                FileStream jacob = File.Open("deletefriend.txt", FileMode.Open);
+                jacob.SetLength(0);
+                jacob.Close();
+            }
+            else if (comm == "block ")
+            {
+                finder = looker.Substring(count);
+                StreamReader blocker = new StreamReader("Users.txt");
+                string troll = blocker.ReadLine();
+                while (troll != null)
+                {
+                    if (troll == finder)
+                    {
+                        File.AppendAllText("block.txt", troll);
+                    }
+                    troll = blocker.ReadLine();
+                }
+                blocker.Close();
+                textBox1.Text = "";
+            }
+            else if (comm == "unblock ")
+            {
+                finder = looker.Substring(count);
+                StreamReader file = new StreamReader("block.txt");
+                string undo = file.ReadLine();
+                while (undo != null)
+                {
+                    if (undo != finder)
+                    {
+                        File.AppendAllText("unblock.txt", undo);
+                    }
+                    undo = file.ReadLine();
+                }
+                file.Close();
+
+                FileStream fileStream = File.Open("block.txt", FileMode.Open);
+                fileStream.SetLength(0);
+                fileStream.Close();
+
+                StreamReader newfile = new StreamReader("unblock.txt");
+                string restore = newfile.ReadLine();
+                while (restore != null)
+                {
+
+                    File.AppendAllText("MyFriends.txt", restore);
+                    restore = newfile.ReadLine();
+                }
+                newfile.Close();
+                textBox1.Text = "";
+
+                FileStream spinny = File.Open("unblock.txt", FileMode.Open);
+                spinny.SetLength(0);
+                spinny.Close();
+            }
+            */
+            // else
+            // {
+
+            string timestamp = DateTime.Now.ToString("MM dd yyyy H:mm:ss tt");
+
+            string user = "[" + namelin + "]";
+            string line = user + " " + timestamp;
+            string input = textBox1.Text;
+            bool ifblocked = false;
+
+            // Here is the Test Case for 
+
+            StreamReader nwfile = new StreamReader("block.txt");
+            string see = nwfile.ReadLine();
+            while (see != null)
+            {
+                if (namelin == see)
+                {
+                    ifblocked = true;
+                }
+                see = nwfile.ReadLine();
+            }
+            nwfile.Close();
+            if (ifblocked)
+            {
+                MessageBox.Show("You have been Blocked");
+            }
+            else
+            {
+
+                File.AppendAllText("Converse.txt", line);
+                File.AppendAllText("Converse.txt", "\n" + input + "\n\n");
+                textBox1.Text = "";
+                string theline = "\n" + line + "\n" + input + "\n";
+                // string filetext = File.ReadAllText("Converse.txt"); // this refreashes from the start of the text file
+                // string filetext = File.R
+                string texty = theline;
+                richTextBox1.Text = richTextBox1.Text + theline;
+            }
+            //}
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            // its feild wll be populated by function button2_Click(object sender , EventArgs e)
+        }
+        /*
+        private void button1_Click(object sender, EventArgs e)      // this is refreash
+        {
+            //string filetext = File.ReadAllText("Converse.txt");
+            //richTextBox1.Text = filetext;
+
+        }
+        */
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = File.ReadAllText("MyFriends.txt");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string finder = textBox3.Text;
+            StreamReader blocker = new StreamReader("Users.txt");
+            string troll = blocker.ReadLine();
+            while (troll != null)
+            {
+                if (troll == finder)
+                {
+                    File.AppendAllText("block.txt", troll);
+                    File.AppendAllText("block.txt", "\n");
+                }
+                troll = blocker.ReadLine();
+            }
+            blocker.Close();
+            textBox3.Text = "";
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string looker = textBox2.Text;
+            StreamReader file = new StreamReader("Users.txt");
+            string friendly = file.ReadLine();
+            while (friendly != null)
+            {
+                if (looker == friendly)
+                {
+                    File.AppendAllText("MyFriends.txt", looker);
+                    File.AppendAllText("MyFriends.txt", "\n");
+                }
+                friendly = file.ReadLine();
+            }
+            file.Close();
+            textBox2.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FileStream jacob = File.Open("deletefriend.txt", FileMode.Open);
+            jacob.SetLength(0);
+            jacob.Close();
+            string finder = textBox2.Text;
+            StreamReader file = new StreamReader("MyFriends.txt");
+            string deleter = file.ReadLine();
+            while (deleter != null)
+            {
+                if (deleter != finder)
+                {
+                    File.AppendAllText("deletefriend.txt", deleter);
+                    File.AppendAllText("deletefriend.txt", "\n");
+
+                }
+                deleter = file.ReadLine();
+            }
+            file.Close();
+
+            FileStream fileStream = File.Open("MyFriends.txt", FileMode.Open);
+            fileStream.SetLength(0);
+            fileStream.Close();
+
+            StreamReader newfile = new StreamReader("deletefriend.txt");
+            string restore = newfile.ReadLine();
+            while (restore != null)
+            {
+
+                File.AppendAllText("MyFriends.txt", restore);
+                File.AppendAllText("MyFriends.txt", "\n");
+                restore = newfile.ReadLine();
+            }
+            newfile.Close();
+            textBox2.Text = "";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            FileStream spinny = File.Open("unblock.txt", FileMode.Open);
+            spinny.SetLength(0);
+            spinny.Close();
+
+            string finder = textBox3.Text;
+            StreamReader file = new StreamReader("block.txt");
+            string undo = file.ReadLine();
+            while (undo != null)
+            {
+                if (undo != finder)
+                {
+                    File.AppendAllText("unblock.txt", undo);
+                    File.AppendAllText("unblock.txt", "\n");
+                }
+                undo = file.ReadLine();
+            }
+            file.Close();
+
+            FileStream fileStream = File.Open("block.txt", FileMode.Open);
+            fileStream.SetLength(0);
+            fileStream.Close();
+
+            StreamReader newfile = new StreamReader("unblock.txt");
+            string restore = newfile.ReadLine();
+            while (restore != null)
+            {
+
+                File.AppendAllText("block.txt", restore);
+                File.AppendAllText("block.txt", "\n");
+                restore = newfile.ReadLine();
+            }
+            newfile.Close();
+            textBox3.Text = "";
+
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = File.ReadAllText("lobbies.txt");
+            textBox1.Text = "Write LobbyID here, then press create to make a lobby or Join to join lobby";
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            string finder = textBox1.Text;
+
+            StreamReader newfile = new StreamReader("lobbies.txt");
+            string restore = newfile.ReadLine();
+            string com;
+            int count = 0;
+            string lobb;
+            while (restore != null)
+            {
+                
+                for (int i = 0; i < restore.Length; i++)
+                {
+                    com = restore.Substring(0, i);
+                    if (finder == com)
+                    {
+                       
+                        break;
+                    }
+                    count++;
+                }
+                lobb = restore.Substring(0, count);
+                if (finder == lobb)
+                { 
+                    break;
+                }
+                restore = newfile.ReadLine();
+            }
+          
+
+            File.AppendAllText("Converse.txt",restore);
+            File.AppendAllText("Converse.txt", "\n");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string finder = textBox1.Text;
+           
+
+
+            File.AppendAllText("lobbies.txt", finder + ": " + namelin + ", " + DateTime.Now.ToString("MM dd yyyy HH:mm:ss tt"));
+            File.AppendAllText("lobbies.txt", "\n");
+            
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            FileStream fileStream = File.Open("Converse.txt", FileMode.Open);
+
+            fileStream.SetLength(0);
+            fileStream.Close();
+
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
